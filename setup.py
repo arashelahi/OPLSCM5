f0b3c973003db596b722990ef6150224f2739488
arashelahi <45706342+arashelahi@users.noreply.github.com>
2025-05-07 18:15:15 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="CNCstruc",
+    version="0.1",
+    packages=find_packages(),  # Automatically finds and includes all sub-packages
+    install_requires=[          # List dependencies if needed
+    ],
+)
